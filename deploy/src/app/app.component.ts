2d11f075daad77b99c6957d5f1b8e4ab14e37520
mavcr <mianvacrro@gmail.com>
2024-03-05 19:59:16 +0000

    Update app.component.ts with imports and HttpClient usage

--- deploy/src/app/app.component.ts
+++ deploy/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'deploy';
+
+  test: {} = {};
+
+  _httpClient: HttpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this._httpClient
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe((data) => {
+        this.test = data;
+      });
+  }
 }
